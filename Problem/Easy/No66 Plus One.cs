42cd4ae5a0d7ea803853849714716dd42d9722b0
jyuankai0919 <87299246+jyuankai0919@users.noreply.github.com>
2024-02-03 16:38:27 +0000

    update new exercise

--- Problem/Easy/No66 Plus One.cs
+++ Problem/Easy/No66 Plus One.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace LeedCodeExercise.Problem.Easy
 {
@@ -13,33 +14,72 @@ namespace LeedCodeExercise.Problem.Easy
         {
             public int[] PlusOne(int[] digits)
             {
-                if(digits.Length == 0) return Array.Empty<int>();
-                var index = digits.Length -1 ;
-                int x = 0; 
-                int[] result = Array.Empty<int>();
-                do
+                // if (digits.Length == 0) return Array.Empty<int>();
+                // var index = digits.Length - 1;
+                // int x = 0;
+                // int[] result = Array.Empty<int>();
+
+                // while (index >= 0)
+                // {
+                //     if (index == digits.Length - 1)
+                //     {
+                //         x = digits[index] + 1;
+                //     }
+                //     else
+                //     {
+                //         x = digits[index];
+                //     }
+
+
+                //     if (x == 10)
+                //     {
+                //         digits[index] = 0;
+
+                //         if (index == 0)
+                //         {
+                //             result = new int[digits.Length + 1];
+                //             result[0] = 1;
+                //             for (int i = 0; i < digits.Length; i++)
+                //             {
+                //                 result[i + 1] = digits[i];
+                //             }
+                //             break;
+                //         }
+                //         else
+                //         {
+                //             digits[index - 1] += 1;
+                //         }
+                //     }
+                //     else
+                //     {
+                //         digits[index] = x;
+                //     }
+                //     result = digits;
+                //     index--;
+                // }
+
+                // return result;
+
+                for (int i = digits.Length - 1; i >= 0; i--)
                 {
-                    if(digits[index] == 9 && index == 0)
+                    if (digits[i] < 9)
                     {
-                        result = new int[digits.Length + 1];
-                        
+                        digits[i]++;
+                        return digits;
                     }
+                    digits[i] = 0;
+                }
 
-
-                    x = digits[index];
-                    if ( x == 10)
-                    {
-
-
-                    }
-                } while (true);
+                digits = new int[digits.Length + 1];
+                digits[0] = 1;
+                return digits;
             }
         }
 
 
         public void Test()
         {
-            Console.WriteLine(new Solution().PlusOne(new int[] { 1, 2, 3 }));
+            Console.WriteLine(new Solution().PlusOne(new int[] { 9, 9, 9, 9, 9, 8, 9, 9, 9, 9, 9, 9, 9 }));
         }
     }
 }
